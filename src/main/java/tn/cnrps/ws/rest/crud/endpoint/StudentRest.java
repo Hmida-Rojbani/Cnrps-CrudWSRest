4b713dce01ffe276db0d40bb594426418f45b0cb
HmidaRojbani <hmida.rojbani@fst.utm.tn>
2020-09-07 13:10:30 +0100

    create student

--- src/main/java/tn/cnrps/ws/rest/crud/endpoint/StudentRest.java
+++ src/main/java/tn/cnrps/ws/rest/crud/endpoint/StudentRest.java
@@ -3,6 +3,9 @@ package tn.cnrps.ws.rest.crud.endpoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +22,16 @@ public class StudentRest {
 	@GetMapping("/id/{id}")
 	public Student getStudentWithId(@PathVariable("id")int id) {
 		return service.getSutudentById(id);
+	}
+	
+	@PostMapping()
+	public Student createStudent(@RequestBody Student std) {
+		return service.addStudent(std);
+	}
+	
+	@PutMapping("/id/{id}")
+	public Student updateStudent(@PathVariable("id")int id,@RequestBody Student newStd) {
+		/// complete 
 	}
 
 }
--- src/main/java/tn/cnrps/ws/rest/crud/services/StudentService.java
+++ src/main/java/tn/cnrps/ws/rest/crud/services/StudentService.java
@@ -1,5 +1,7 @@
 package tn.cnrps.ws.rest.crud.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +21,15 @@ public class StudentService {
 		}
 		
 		return null;
+	}
+	
+	public Student addStudent(Student std) {
+		List<Student> students = repos.getStudents();
+		int id = students.get(students.size()-1).getId();
+		
+		std.setId(id+1);
+		students.add(std);
+		return std;
 	}
 
 }
